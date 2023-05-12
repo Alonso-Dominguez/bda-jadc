9717304df0f9b40946e7916a4c9879aa295ee9d0
JOS-ALONSOALONS <19628906-JOS-ALONSOALONS@users.noreply.replit.com>
2023-05-12 13:47:27 +0000

    Initial commit

---
+++ main.sql
@@ -0,0 +1,15 @@
+CREATE TABLE personas(
+  id_persona       INTEGER          PRIMARY KEY,
+  nombre           varchar(50)      NOT NULL,
+  primer_apellido  varchar(50)      NOT NULL,
+  segundo_apellido varchar(50)      NOT NULL,
+  email            varchar(50)      NOT NULL
+  );
+
+
+INSERT INTO personas(nombre, primer_apellido, segundo_apellido, email) VALUES
+  ('juan', 'castro', 'lopez', 'juanito@gmail.com'),
+  ('carlos', 'sanchez', 'cruz', 'carlos@gmail.com'),
+  ('alfredo', 'morales', 'castillo', 'miguel@gmail.com');
+
+SELECT * FROM personas;
